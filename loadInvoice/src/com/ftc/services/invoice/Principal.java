826bf2d8d38c836454c40232058cc54a468ddaeb
Captain Omash <cafaray@gmail.com>
2014-03-21 10:31:17 -0600

    cambio en la logica de carga de facturas

--- loadInvoice/src/com/ftc/services/invoice/Principal.java
+++ loadInvoice/src/com/ftc/services/invoice/Principal.java
@@ -5,9 +5,7 @@ import java.io.File;
 import java.io.FileOutputStream;
 import java.io.FilenameFilter;
 import java.io.IOException;
-import java.text.SimpleDateFormat;
 import java.util.ArrayList;
-import java.util.Calendar;
 import java.util.List;
 
 import javax.xml.parsers.DocumentBuilder;
@@ -27,18 +25,24 @@ public class Principal {
 	public static void main(String[] args) {
 		String folderIn = "/tmp/";
 		String folderOut = folderIn;
+		String yfileName = "hashPipe";
 		try {
-			if (args.length >= 1) {
-				folderIn = args[0];
-				folderOut = folderIn;
-			} else if (folderIn.length() <= 0) {
-				System.out
-						.println("No se detecto una ruta para localizar documentos. Usa loadInvoice <directorio_facturas> [directorio_salida]");
-				System.exit(1);
+			if (args.length > 0) {
+				for (String arg:args){
+					if(arg.startsWith("--dir=")){
+						folderIn = arg.substring(arg.indexOf("=")+1);
+						System.out.println("Estableciendo folder de trabajo en: "+folderIn);
+					}else if(arg.startsWith("--file=")){
+						yfileName = arg.substring(arg.indexOf("=")+1);
+						System.out.println("Estableciendo nombre de archivo como: "+yfileName);
+					}else{
+						System.out.printf("No se reconoce el argumento %s. Usa --dir=[directorio] o --file=[nombre archivo]");
+						System.exit(1);
+					}
+				}
 			}
-			if (args.length == 2) {
-				folderOut = args[1];
-			}
+			folderOut = folderIn;
+			
 			File folder = new File(folderIn);
 			System.out.println("La ruta al archivo " + folderIn + " existe: "
 					+ folder.exists());
@@ -56,7 +60,7 @@ public class Principal {
 					registros.add(cabecera);
 				}
 
-				int resultado = escribeSalida(folderOut, registros);
+				int resultado = escribeSalida(folderOut, yfileName, registros);
 				if (resultado < 0) {
 					System.out
 							.println("Hubo un error durante la escritura de los registros. Revise el log de salida.");
@@ -74,17 +78,18 @@ public class Principal {
 		}
 	}
 
-	private static int escribeSalida(String folder, List<Cabecera> registros)
+	private static int escribeSalida(String folder, String yfileName, List<Cabecera> registros)
 			throws IOException {
 		File file = new File(folder);
 		int iregistros = 0;
 		if (file.exists()) {
-			Calendar hoy = Calendar.getInstance();
-			SimpleDateFormat format = new SimpleDateFormat("yyyymmdd-hhmm");
+			//Calendar hoy = Calendar.getInstance();
+			//SimpleDateFormat format = new SimpleDateFormat("yyyymmdd-hhmm");
 
-			String yfileName = folder + File.separator
-					+ format.format(hoy.getTime()) + ".csv";
-			File yfile = new File(yfileName);
+			//String yfileName = folder + File.separator
+			//		+ format.format(hoy.getTime()) + ".csv";
+			yfileName+=".csv";
+			File yfile = new File(new String(folder+yfileName));
 			if (yfile.exists()) {
 				System.out.printf("El archivo %s existe, sera eliminado: ",
 						yfileName);
@@ -94,16 +99,19 @@ public class Principal {
 			System.out.printf("Archivo %s listo para escribir: ", yfileName);
 			if (yfile.createNewFile()) {
 				System.out.println("[OK]");
-				FileOutputStream fos = new FileOutputStream(yfile);
-				DataOutputStream dos = new DataOutputStream(fos);
-				dos.writeChars(Cabecera.titulosCommaSeparateValues()
-						+ System.lineSeparator());
-				for (Cabecera registro : registros) {
-					dos.writeChars(registro.toCommaSeparateValues()
+				try (FileOutputStream fos = new FileOutputStream(yfile);
+						DataOutputStream dos = new DataOutputStream(fos)) {
+					dos.writeChars(Cabecera.titulosCommaSeparateValues()
 							+ System.lineSeparator());
-					iregistros++;
+					for (Cabecera registro : registros) {
+						dos.writeChars(registro.toCommaSeparateValues()
+								+ System.lineSeparator());
+						iregistros++;
+					}
+					// fos.close();
+				} catch (IOException ioe) {
+					throw ioe;
 				}
-				fos.close();
 			}
 			return iregistros;
 		} else {
@@ -116,7 +124,7 @@ public class Principal {
 	private static Cabecera procesaXML(String file) throws IOException,
 			ParserConfigurationException, SAXException {
 		File fXmlFile = new File(file);
-		
+
 		DocumentBuilderFactory dbFactory = DocumentBuilderFactory.newInstance();
 		DocumentBuilder dBuilder = dbFactory.newDocumentBuilder();
 		Document doc = dBuilder.parse(fXmlFile);
@@ -128,20 +136,21 @@ public class Principal {
 		System.out.println("Root element :"
 				+ doc.getDocumentElement().getNodeName());
 		boolean esCFDI = true;
-		esCFDI = doc.getDocumentElement().getNodeName().toUpperCase().startsWith("CFDI");
-		System.out.println("El documento es CFDI? "+esCFDI);
-		String prefijo = (esCFDI?"cfdi:":"");
-		
-		NodeList nList = doc.getElementsByTagName(prefijo+ "Comprobante");
-		
+		esCFDI = doc.getDocumentElement().getNodeName().toUpperCase()
+				.startsWith("CFDI");
+		System.out.println("El documento es CFDI? " + esCFDI);
+		String prefijo = (esCFDI ? "cfdi:" : "");
+
+		NodeList nList = doc.getElementsByTagName(prefijo + "Comprobante");
+
 		System.out.println("----------------------------");
 		Cabecera cabecera = new Cabecera();
 		for (int temp = 0; temp < nList.getLength(); temp++) {
 			Node nNode = nList.item(temp);
 			System.out.println("\nCurrent Element :" + nNode.getNodeName());
 			if (nNode.getNodeType() == Node.ELEMENT_NODE) {
-				Element eElement = (Element) nNode;				
-				
+				Element eElement = (Element) nNode;
+
 				System.out.println("Serie : " + eElement.getAttribute("serie"));
 				System.out.println("Folio : " + eElement.getAttribute("folio"));
 				System.out.println("Fecha : " + eElement.getAttribute("fecha"));
@@ -175,7 +184,7 @@ public class Principal {
 			}
 		}
 
-		nList = doc.getElementsByTagName(prefijo+ "Emisor");
+		nList = doc.getElementsByTagName(prefijo + "Emisor");
 		System.out.println("----------------------------");
 		for (int temp = 0; temp < nList.getLength(); temp++) {
 			Node nNode = nList.item(temp);
@@ -191,13 +200,13 @@ public class Principal {
 			}
 		}
 
-		nList = doc.getElementsByTagName(prefijo+ "Receptor");
+		nList = doc.getElementsByTagName(prefijo + "Receptor");
 		System.out.println("----------------------------");
-		for(int temp=0;temp<nList.getLength();temp++){
+		for (int temp = 0; temp < nList.getLength(); temp++) {
 			Node nNode = nList.item(temp);
 			System.out.println("\nCurrent Element :" + nNode.getNodeName());
-			if(nNode.getNodeType()==Node.ELEMENT_NODE){
-				Element eElement = (Element)nNode;
+			if (nNode.getNodeType() == Node.ELEMENT_NODE) {
+				Element eElement = (Element) nNode;
 				System.out.println("RFC : " + eElement.getAttribute("rfc"));
 				System.out.println("Nombre : "
 						+ eElement.getAttribute("nombre"));
@@ -205,8 +214,8 @@ public class Principal {
 				cabecera.setNombreReceptor(eElement.getAttribute("nombre"));
 			}
 		}
-		
-		nList = doc.getElementsByTagName(prefijo+ "Impuestos");
+
+		nList = doc.getElementsByTagName(prefijo + "Impuestos");
 		System.out.println("----------------------------");
 		for (int temp = 0; temp < nList.getLength(); temp++) {
 			Node nNode = nList.item(temp);
@@ -220,43 +229,50 @@ public class Principal {
 				// eElement.getElementsByTagName("firstname").item(0).getTextContent()
 			}
 		}
-		
-		nList = doc.getElementsByTagName(prefijo+ "Traslados");
+
+		nList = doc.getElementsByTagName(prefijo + "Traslados");
 		System.out.println("----------------------------");
 		for (int temp = 0; temp < nList.getLength(); temp++) {
 			Node nNode = nList.item(temp);
-			System.out.printf("\nCurrent Element : %s-%s%n", nNode.getNodeType() , nNode.getNodeName());
-			
+			System.out.printf("\nCurrent Element : %s-%s%n",
+					nNode.getNodeType(), nNode.getNodeName());
+
 			if (nNode.getNodeType() == Node.ELEMENT_NODE) {
-				//Element eElement = (Element) nNode;
-				NodeList nodes = nNode.getChildNodes();				
-				System.out.println("Impuestos encontrados : "+ nodes.getLength());				
-				for(int ximpuesto = 0; ximpuesto<nodes.getLength();ximpuesto++){
+				// Element eElement = (Element) nNode;
+				NodeList nodes = nNode.getChildNodes();
+				System.out.println("Impuestos encontrados : "
+						+ nodes.getLength());
+				for (int ximpuesto = 0; ximpuesto < nodes.getLength(); ximpuesto++) {
 					Node nNodeImpuesto = nodes.item(ximpuesto);
-					
-					if(nNodeImpuesto.getNodeType()==Node.ELEMENT_NODE){
-						System.out.printf("\nCurrent Element : %s-%s%n", nNodeImpuesto.getNodeType() , nNodeImpuesto.getNodeName());
-						Element xElement = (Element)nNodeImpuesto;
+
+					if (nNodeImpuesto.getNodeType() == Node.ELEMENT_NODE) {
+						System.out.printf("\nCurrent Element : %s-%s%n",
+								nNodeImpuesto.getNodeType(),
+								nNodeImpuesto.getNodeName());
+						Element xElement = (Element) nNodeImpuesto;
 						String impuesto, tasa, importe;
 						impuesto = xElement.getAttribute("impuesto");
 						tasa = xElement.getAttribute("tasa");
 						importe = xElement.getAttribute("importe");
-						System.out.printf("impuesto = %s, tasa = %s, importe = %s %n", impuesto, tasa, importe);
-						if (impuesto.equals(Cabecera.IMPUESTO_IVA)){
+						System.out.printf(
+								"impuesto = %s, tasa = %s, importe = %s %n",
+								impuesto, tasa, importe);
+						if (impuesto.equals(Cabecera.IMPUESTO_IVA)) {
 							cabecera.setIva_strTasa(tasa);
 							cabecera.setIva_strImporte(importe);
-						}else if (impuesto.equals(Cabecera.IMPUESTO_IEPS)){
+						} else if (impuesto.equals(Cabecera.IMPUESTO_IEPS)) {
 							cabecera.setIeps_strTasa(tasa);
-							cabecera.setIeps_strImporte(importe);							
-						}else{
-							System.out.println("Impuesto no detectado en el objeto: "+impuesto);
+							cabecera.setIeps_strImporte(importe);
+						} else {
+							System.out
+									.println("Impuesto no detectado en el objeto: "
+											+ impuesto);
 						}
 					}
 				}
 			}
 		}
-		
-		
+
 		nList = doc.getElementsByTagName("tfd:TimbreFiscalDigital");
 		System.out.println("----------------------------");
 		for (int temp = 0; temp < nList.getLength(); temp++) {
